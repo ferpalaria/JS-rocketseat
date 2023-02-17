5efb8982e349008a927246eb2aec93a5eecf4709
Fernanda Palaria <fernanda_palaria@terceiros.sicredi.com.br>
2023-02-17 00:10:49 -0300

    intro JS

---
+++ main.js
@@ -0,0 +1,26 @@
+const randomNumber = Math.round(Math.random() * 10)
+
+let xAttempts = 1
+
+function handleClick(event) {
+    event.preventDefault()
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    console.log(inputNumber.value)
+    console.log(randomNumber)
+
+    if (Number(inputNumber.value) == randomNumber) {
+        document.querySelector(".screen1").classList.add("hide")
+        document.querySelector(".screen2").classList.remove("hide")
+
+        document
+            .querySelector(".screen2 h2")
+            .innerHTML = `Acertou em ${xAttempts} tentativas`
+    }
+
+    xAttempts++
+}
+
+    // Eventos
+
