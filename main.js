39a6b200e5d1518efb9405f0def08525c61471d8
Fernanda Palaria <fernanda_palaria@terceiros.sicredi.com.br>
2023-02-17 00:53:55 -0300

    refatoração

--- main.js
+++ main.js
@@ -3,23 +3,21 @@ const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
-
 let xAttempts = 1
 
-// função callback
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', resetClick)
+
+
 function handleTryClick(event) {
-    // para não tentar enviar os dados do form (pq o botão está dentro do form)
-    event.preventDefault()
+    event.preventDefault()  // para não tentar enviar os dados do form (pq o botão está dentro do form)
 
     const inputNumber = document.querySelector("#inputNumber")
 
-    console.log(randomNumber)
-
     if (Number(inputNumber.value) == randomNumber) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        toggleScreen()
 
-        // Alternativa reaproveitando a variável 
         screen2.querySelector("h2").innerHTML =
             `Acertou em ${xAttempts} tentativas`
     }
@@ -27,19 +25,15 @@ function handleTryClick(event) {
     xAttempts++
 }
 
-// Função callback
-btnTry.addEventListener('click', handleTryClick)
 
-// Com função anonima
-btnReset.addEventListener('click', function () {
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-
+function resetClick() {
+    toggleScreen()
     xAttempts = 1
-})
+}
 
 function toggleScreen() {
-    
+    screen1.classList.remove("hide")
+    screen2.classList.add("hide")
 }
 
 
